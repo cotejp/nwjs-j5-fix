3bd5e903798ae480eb1ff43a5649c25481f9bc5e
Jean-Philippe Côté <jp@djip.co>
2020-03-13 10:22:24 -0400

    Improve detection of NW.js > 0.13

--- nwjs-j5-fix.js
+++ nwjs-j5-fix.js
@@ -1,13 +1,14 @@
-var obj = {
+let obj = {
 
   fix: function() {
 
-    if (!process.versions.nw || parseFloat(process.versions.nw) < 0.13) {
-      throw "This module is only relevant when run under NW.js v0.13 or higher."
+
+    if (!process || !process.versions.nw || parseFloat(process.versions.nw) < 0.13) {
+      return;
     }
 
-    var Readable = nw.require("stream").Readable;
-    var util = nw.require("util");
+    let Readable = nw.require("stream").Readable;
+    let util = nw.require("util");
 
     function MyStream(opt) {
       Readable.call(this, opt);
